5e90b5c6c476d76e1e8de6298bacabc0363989c0
dtutino <d.tutino@solvingteam.it>
2021-11-23 10:28:48 +0100

    First commit

---
+++ src/main/java/it/prova/gestionedottore/GestionedottoreApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionedottore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionedottoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionedottoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionedottore/GestionedottoreApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionedottore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionedottoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
